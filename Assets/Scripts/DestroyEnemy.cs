0432ee6b0cb1358a2194dd64a0f52ca0f7ba40b9
waterpolord <robertgarcia341@gmail.com>
2022-06-11 18:34:49 -0400

    juego de generador de esferas

---
+++ Assets/Scripts/DestroyEnemy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEnemy : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        Destroy(other.gameObject);
+    }
+
+
+}
